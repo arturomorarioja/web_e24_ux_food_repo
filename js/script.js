53782b8b83442861a0fb95201e50f41d1166fdd3
Arturo Mora-Rioja <arturomora@gmail.com>
2024-09-12 10:23:23 +0200

    Event listener added for the recipe list

--- js/script.js
+++ js/script.js
@@ -15,9 +15,49 @@ const recipes = [
     }
 ];
 
+// We grab the container for the recipe card
+const recipeInfoSection = document.querySelector('#recipe-info');
+
+recipes.forEach(function(recipe) {
+    const recipeLinkText = document.createTextNode(recipe.name);
+    const recipeLink = document.createElement('a');
+    recipeLink.append(recipeLinkText);
+    recipeLink.setAttribute('href', '#');
+
+    recipeLink.addEventListener('click', function() {
+        // We empty the recipe info section
+        recipeInfoSection.innerHTML = '';
+
+        // We create the h2
+        const recipeH2Text = document.createTextNode(recipe.name);
+        const recipeH2 = document.createElement('h2');
+        recipeH2.append(recipeH2Text);
+
+        // We create the header wrapping the h2
+        const recipeHeader = document.createElement('header');
+        recipeHeader.append(recipeH2);
+
+        // We create the paragraph with the description
+        const recipeDescriptionText = document.createTextNode(recipe.description);
+        const recipeDescription = document.createElement('p');
+        recipeDescription.append(recipeDescriptionText);
+
+        // Both the header and the description are added to the recipe info area
+        recipeInfoSection.append(recipeHeader);
+        recipeInfoSection.append(recipeDescription);
+    });
+
+    const recipeListItem = document.createElement('li');
+    recipeListItem.append(recipeLink);
+
+    document.querySelector('section#recipe-list > ul').append(recipeListItem);
+});
+
+/*
 recipes.forEach(function(recipe) {
     const listItem = `
         <li><a href="#">${recipe.name}</a></li>
     `;
     document.querySelector('section#recipe-list > ul').innerHTML += listItem;
 });
+*/
