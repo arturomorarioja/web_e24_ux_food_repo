d1b31f96a03c70e3ba22fdfb71dfbcbdb8d00d9c
Arturo Mora-Rioja <arturomora@gmail.com>
2024-09-26 11:44:02 +0200

    Recipe cards created in homepage

--- js/script.js
+++ js/script.js
@@ -5,52 +5,64 @@ const NUM_RECIPES_TO_SHOW = 10;
 const MAX_INGREDIENTS = 20;
 
 // We grab the container for the recipe card
-const recipeInfoSection = document.querySelector('#recipe-info');
+const recipeInfoSection = document.querySelector('#recipe-cards');
 
 const handleRecipe = function(data) {
     const recipe = data.meals[0];
 
-    const recipeLink = document.createElement('a');
-    recipeLink.innerText = recipe.strMeal;
-    recipeLink.setAttribute('href', '#');
+    const recipeCard = document.createElement('article');
+    recipeCard.innerHTML = `
+        <header>
+            <h3><a href="recipe.htm?id=${recipe.idMeal}">${recipe.strMeal}</a></h3>
+        </header>
+        <a href="recipe.htm?id=${recipe.idMeal}"><img src="${recipe.strMealThumb}" alt=""></a>
+        <div>
+            <p class="pill foodType">${recipe.strCategory}</p>
+            <p class="pill area">${recipe.strArea}</p>
+        </div>
+    `;
 
-    recipeLink.addEventListener('click', function() {
-        // We empty the recipe info section
-        let recipeInfo = `
-            <header>
-                <h2>${recipe.strMeal}</h2>
-            </header>
-            <img src="${recipe.strMealThumb}" alt="">
-            <p>${recipe.strInstructions}</p>
-            <ul>
-        `;
-        for (let index = 0; index < MAX_INGREDIENTS; index++) {
-            const ingredient = recipe[`strIngredient${index + 1}`];
-            if (ingredient !== null && ingredient !== '') {
-                const measure = recipe[`strMeasure${index + 1}`];
-                recipeInfo += `<li>${ingredient}, ${measure}</li>`;
-            }
-        }
-        recipeInfo += '</ul>';
-        let youtubeID = recipe.strYoutube;
-        youtubeID = youtubeID.substring(youtubeID.length - 11);
-        recipeInfo += `
-            <iframe 
-                src="https://www.youtube.com/embed/${youtubeID}?si=MO0O8ATQ_yYp_1wR" 
-                title="YouTube video player" 
-                frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                referrerpolicy="strict-origin-when-cross-origin" 
-                allowfullscreen>
-            </iframe>
-        `;
-        recipeInfoSection.innerHTML = recipeInfo;
-    });
+    document.querySelector('section#recipe-cards').append(recipeCard);        
 
-    const recipeListItem = document.createElement('li');
-    recipeListItem.append(recipeLink);
+    // const recipeLink = document.createElement('a');
+    // recipeLink.innerText = recipe.strMeal;
+    // recipeLink.setAttribute('href', '#');
 
-    document.querySelector('section#recipe-list > ul').append(recipeListItem);        
+    // recipeLink.addEventListener('click', function() {
+    //     // We empty the recipe info section
+    //     let recipeInfo = `
+    //         <header>
+    //             <h2>${recipe.strMeal}</h2>
+    //         </header>
+    //         <img src="${recipe.strMealThumb}" alt="">
+    //         <p>${recipe.strInstructions}</p>
+    //         <ul>
+    //     `;
+    //     for (let index = 0; index < MAX_INGREDIENTS; index++) {
+    //         const ingredient = recipe[`strIngredient${index + 1}`];
+    //         if (ingredient !== null && ingredient !== '') {
+    //             const measure = recipe[`strMeasure${index + 1}`];
+    //             recipeInfo += `<li>${ingredient}, ${measure}</li>`;
+    //         }
+    //     }
+    //     recipeInfo += '</ul>';
+    //     let youtubeID = recipe.strYoutube;
+    //     youtubeID = youtubeID.substring(youtubeID.length - 11);
+    //     recipeInfo += `
+    //         <iframe 
+    //             src="https://www.youtube.com/embed/${youtubeID}?si=MO0O8ATQ_yYp_1wR" 
+    //             title="YouTube video player" 
+    //             frameborder="0" 
+    //             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    //             referrerpolicy="strict-origin-when-cross-origin" 
+    //             allowfullscreen>
+    //         </iframe>
+    //     `;
+    //     recipeInfoSection.innerHTML = recipeInfo;
+    // });
+
+    // const recipeListItem = document.createElement('li');
+    // recipeListItem.append(recipeLink);
 }
 
 const handleAPIError = function(response) {
@@ -72,24 +84,3 @@ for (let index = 0; index < NUM_RECIPES_TO_SHOW; index++) {
         `;
     })
 }
-
-// recipes.forEach(function(recipe) {
-//     const recipeLink = document.createElement('a');
-//     recipeLink.innerText = recipe.name;
-//     recipeLink.setAttribute('href', '#');
-
-//     recipeLink.addEventListener('click', function() {
-//         // We empty the recipe info section
-//         recipeInfoSection.innerHTML = `
-//             <header>
-//                 <h2>${recipe.name}</h2>
-//             </header>
-//             <p>${recipe.description}</p>
-//         `;
-//     });
-
-//     const recipeListItem = document.createElement('li');
-//     recipeListItem.append(recipeLink);
-
-//     document.querySelector('section#recipe-list > ul').append(recipeListItem);
-// });
