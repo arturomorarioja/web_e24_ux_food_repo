808912f7425359307c16e3ecf0fe3c126fa0b880
Arturo Mora-Rioja <arturomora@gmail.com>
2024-10-10 15:20:27 +0200

    Sign up and Log in implemented

--- js/common.js
+++ js/common.js
@@ -1,8 +1,13 @@
 export const baseUrl = 'https://www.themealdb.com/api/json/v1/1';
+export const baseUserUrl = 'http://localhost:8001'
 
 export const handleAPIError = function(response) {
     if (response.ok) {
         return response.json();
     }
     console.log('There was an error');
+}
+
+export const handleAPIResponseError = (error = 'Generic error') => {
+    alert('There was an error: ' + error);
 }
---
+++ js/login.js
@@ -0,0 +1,27 @@
+import { baseUserUrl, handleAPIResponseError } from './common.js';
+
+document.querySelector('#frmLogin').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = e.target.txtEmail.value.trim();
+    const password = e.target.txtPassword.value.trim();
+
+    const params = new URLSearchParams();
+    params.append('email', email);
+    params.append('password', password);
+
+    fetch(`${baseUserUrl}/validation`, {
+        method: 'POST',
+        body: params
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (Object.keys(data).includes('user_id')) {
+            alert('Login was successful');
+            window.location.href = 'index.html';
+        } else {
+            handleAPIResponseError(data.error);
+        }
+    })
+    .catch(handleAPIResponseError);
+});
